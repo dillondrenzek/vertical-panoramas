dc377d74c5599e1162f8b0785bd8d1353af92e74
Dillon Drenzek <dr.enzek@gmail.com>
2017-03-05 12:20:32 -0800

    attempting to fix the missing process variable

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,6 @@ import { OpaqueToken } from '@angular/core';
 
 declare var process: any;
 
-
 /**
  * EnvConfig
  */
@@ -21,15 +20,20 @@ export let ENV_CONFIG = new OpaqueToken('ENV_CONFIG');
  * Config Builder
  */
 function getEnvConfig(): EnvConfig {
-
-  console.warn('process.env', process.env);
-  let appPath = (process && process.env) ? process.env.APP_PATH : 'http://localhost:8080';
   let config: EnvConfig = {
-    appPath: appPath,
-    appPort: 5000
+    appPath: 'http://localhost:8080',
+    appPort: 8080
   };
+
+  if (process && process.env) {
+    console.warn('process.env', process.env);
+
+    if (process.env.APP_PATH) config['appPath'] = process.env.APP_PATH;
+    if (process.env.PORT) config['appPort'] = process.env.PORT;
+  }
+
   console.warn('config', config);
-  return
+  return config;
 }
 
 /**
